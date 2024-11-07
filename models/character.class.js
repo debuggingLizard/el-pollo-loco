6ce0dee3e64a2ce0f4889fd25057e1b4334b0952
Lisa-Marie Maliga <lisa.maliga@outlook.com>
2024-11-07 23:11:45 +0100

    animated objects

--- models/character.class.js
+++ models/character.class.js
@@ -1,13 +1,31 @@
 class Character extends MovableObject {
-    height = 320;
-    width = 150;
-    y = 110;
+  height = 320;
+  width = 150;
+  y = 110;
+  IMAGES_WALKING = [
+    "../img/2_character_pepe/2_walk/W-21.png",
+    "../img/2_character_pepe/2_walk/W-22.png",
+    "../img/2_character_pepe/2_walk/W-23.png",
+    "../img/2_character_pepe/2_walk/W-24.png",
+    "../img/2_character_pepe/2_walk/W-25.png",
+    "../img/2_character_pepe/2_walk/W-26.png",
+  ];
 
-    constructor() {
-        super().loadImage('../img/2_character_pepe/2_walk/W-21.png');
-    }
+  constructor() {
+    super().loadImage("../img/2_character_pepe/2_walk/W-21.png");
+    this.loadImages(this.IMAGES_WALKING);
+    // this.animate();
+  }
 
-    jump() {
+  animate() {
+    setInterval(() => {
+      let i = this.currentImage % this.IMAGES_WALKING.length;
+      // 0, 1, 2, 3, 4, 5, 0, 1, 2, ....
+      let path = this.IMAGES_WALKING[i];
+      this.img = this.imageCache[path];
+      this.currentImage++;
+    }, 150);
+  }
 
-    }
+  jump() {}
 }
--- models/movable-object.class.js
+++ models/movable-object.class.js
@@ -4,15 +4,30 @@ class MovableObject {
   img;
   height = 150;
   width = 100;
+  imageCache = [];
+  currentImage = 0;
+  speed = 0.15;
 
   loadImage(path) {
     this.img = new Image(); //<img>
     this.img.src = path;
   }
 
+  loadImages(arr) {
+    arr.forEach((path) => {
+      let img = new Image();
+      img.src = path;
+      this.imageCache[path] = img;
+    });
+  }
+
   moveRight() {
     console.log("moving right");
   }
 
-  moveLeft() {}
+  moveLeft() {
+    setInterval(() => {
+      this.x -= this.speed;
+    }, 1000 / 60);
+  }
 }
