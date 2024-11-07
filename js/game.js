a220545421f6812e5bfa88083894fbdfd8c9a772
Lisa-Marie Maliga <lisa.maliga@outlook.com>
2024-11-07 23:47:31 +0100

    keyboard

--- js/game.js
+++ js/game.js
@@ -1,12 +1,47 @@
 let canvas;
 let world;
+let keyboard = new Keyboard();
 
 function init() {
   canvas = document.getElementById("canvas");
-  world = new World(canvas);
+  world = new World(canvas, keyboard);
 
   console.log("my character is: ", world.character); //oder world['character'] = ist beides das gleiche
   console.log("my enemies are: ", world.enemies);
-  
 }
 
+window.addEventListener("keydown", (e) => {
+  if (e.key == 'd' || e.key == 'D') {
+    keyboard.RIGHT = true;
+  }
+  if (e.key == 'a' || e.key == 'A') {
+    keyboard.LEFT = true;
+  }
+  if (e.key == 'w' || e.key == 'W') {
+    keyboard.UP = true;
+  }
+  if (e.key == 's' || e.key == 'S') {
+    keyboard.DOWN = true;
+  }
+  if (e.key == ' ') {
+    keyboard.SPACE = true;
+  }
+});
+
+window.addEventListener("keyup", (e) => {
+  if (e.key == 'd' || e.key == 'D') {
+    keyboard.RIGHT = false;
+  }
+  if (e.key == 'a' || e.key == 'A') {
+    keyboard.LEFT = false;
+  }
+  if (e.key == 'w' || e.key == 'W') {
+    keyboard.UP = false;
+  }
+  if (e.key == 's' || e.key == 'S') {
+    keyboard.DOWN = false;
+  }
+  if (e.key == ' ') {
+    keyboard.SPACE = false;
+  }
+})
