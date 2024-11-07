ab21364d8143df582b59b6560ca4f4ef386758cf
Lisa-Marie Maliga <lisa.maliga@outlook.com>
2024-11-07 16:31:24 +0100

    place chickens

--- models/world.class.js
+++ models/world.class.js
@@ -1,22 +1,39 @@
 class World {
+  character = new Character();
+  enemies = [new Chicken(), new Chicken(), new Chicken()];
+  canvas;
+  ctx;
 
-    character = new Character();
-    enemies = [new Chicken(), new Chicken(), new Chicken()];
-    ctx;
+  constructor(canvas) {
+    this.ctx = canvas.getContext("2d");
+    this.canvas = canvas;
+    this.draw();
+  }
 
-    constructor(canvas) {
-        this.ctx = canvas.getContext('2d');
-        this.draw();
-    }
+  draw() {
+    this.ctx.clearRect(0, 0, this.canvas.width, this.canvas.height);
+    this.ctx.drawImage(
+      this.character.img,
+      this.character.x,
+      this.character.y,
+      this.character.width,
+      this.character.height
+    );
 
-    draw() {
-        this.ctx.drawImage(this.character.img, this.character.x, this.character.y, this.character.width, this.character.height)
-        // for (let i = 0; i < this.enemies.length; i++) {
-        //     this.ctx.drawImage(this.enemies[i].img, this.enemies[i].x + 50, this.enemies[i].y + 50, this.enemies[i].width, this.enemies[i].height)
-        // }
+    this.enemies.forEach((enemy) => {
+      this.ctx.drawImage(
+        enemy.img,
+        enemy.x,
+        enemy.y,
+        enemy.width,
+        enemy.height
+      );
+    });
 
-        this.enemies.forEach(enemy => {
-            this.ctx.drawImage(enemy.img, enemy.x + 50 , enemy.y + 50, enemy.width, enemy.height)
-        });
-    }
+    // draw() wird immer wieder aufgerufen
+    self = this;
+    requestAnimationFrame(function () {
+      self.draw();
+    });
+  }
 }
