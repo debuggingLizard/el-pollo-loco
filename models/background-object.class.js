49de16e58ad206257aaf4fbbd0d06c06070c2f89
Lisa-Marie Maliga <lisa.maliga@outlook.com>
2024-11-07 18:36:09 +0100

    positioning and size of movable objects

--- models/background-object.class.js
+++ models/background-object.class.js
@@ -1,11 +1,11 @@
 class BackgroundObject extends MovableObject {
     width = 720;
-    height = 400;
+    height = 480;
 
     constructor(imagePath, x) {
         super().loadImage(imagePath);
         this.x = x;
-        this.y = 480 - this.height;
+        this.y = 0;
 
     }
 }
--- models/character.class.js
+++ models/character.class.js
@@ -1,4 +1,7 @@
 class Character extends MovableObject {
+    height = 320;
+    width = 150;
+    y = 110;
 
     constructor() {
         super().loadImage('../img/2_character_pepe/2_walk/W-21.png');
