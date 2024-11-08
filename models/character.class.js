f682d74df3cde08f8e241de3e00a667b81e277d3
Lisa-Marie Maliga <lisa.maliga@outlook.com>
2024-11-08 10:54:16 +0100

    pepe can walk

--- models/character.class.js
+++ models/character.class.js
@@ -2,6 +2,7 @@ class Character extends MovableObject {
   height = 320;
   width = 150;
   y = 110;
+  speed = 10;
   IMAGES_WALKING = [
     "../img/2_character_pepe/2_walk/W-21.png",
     "../img/2_character_pepe/2_walk/W-22.png",
@@ -20,16 +21,23 @@ class Character extends MovableObject {
 
   animate() {
     setInterval(() => {
+      if (this.world.keyboard.RIGHT) {
+        this.x += this.speed;
+      }
 
-        if(this.world.keyboard.RIGHT) {
-            let i = this.currentImage % this.IMAGES_WALKING.length;
-            // 0, 1, 2, 3, 4, 5, 0, 1, 2, ....
-            let path = this.IMAGES_WALKING[i];
-            this.img = this.imageCache[path];
-            this.currentImage++;
-        }
-     
-    }, 150);
+      if (this.world.keyboard.LEFT) {
+        this.x -= this.speed;
+      }
+    }, 1000 / 60);
+    setInterval(() => {
+      if (this.world.keyboard.RIGHT || this.world.keyboard.LEFT) {
+        let i = this.currentImage % this.IMAGES_WALKING.length;
+        // 0, 1, 2, 3, 4, 5, 0, 1, 2, ....
+        let path = this.IMAGES_WALKING[i];
+        this.img = this.imageCache[path];
+        this.currentImage++;
+      }
+    }, 50);
   }
 
   jump() {}
