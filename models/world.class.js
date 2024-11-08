f0451103c28a119a0b451d2e9ed2862e61cf254a
Lisa-Marie Maliga <lisa.maliga@outlook.com>
2024-11-08 13:47:12 +0100

    move back ground

--- models/world.class.js
+++ models/world.class.js
@@ -6,11 +6,12 @@ class World {
     new BackgroundObject("../img/5_background/layers/air.png", 0),
     new BackgroundObject("../img/5_background/layers/3_third_layer/1.png", 0),
     new BackgroundObject("../img/5_background/layers/2_second_layer/1.png", 0),
-    new BackgroundObject("../img/5_background/layers/1_first_layer/1.png", 0)
+    new BackgroundObject("../img/5_background/layers/1_first_layer/1.png", 0),
   ];
   canvas;
   ctx;
   keyboard;
+  camera_x = 0;
 
   constructor(canvas, keyboard) {
     this.ctx = canvas.getContext("2d");
@@ -27,10 +28,14 @@ class World {
   draw() {
     this.ctx.clearRect(0, 0, this.canvas.width, this.canvas.height);
 
+    this.ctx.translate(this.camera_x, 0);
+
     this.addObjectsToMap(this.backgroundObjects);
     this.addObjectsToMap(this.clouds);
     this.addObjectsToMap(this.enemies);
     this.addToMap(this.character);
+
+    this.ctx.translate(-this.camera_x, 0);
 
     self = this;
     requestAnimationFrame(function () {
@@ -45,6 +50,12 @@ class World {
   }
 
   addToMap(movableObject) {
+    if (movableObject.otherDirection) {
+      this.ctx.save();
+      this.ctx.translate(movableObject.width, 0);
+      this.ctx.scale(-1, 1);
+      movableObject.x = movableObject.x * -1;
+    }
     this.ctx.drawImage(
       movableObject.img,
       movableObject.x,
@@ -52,5 +63,9 @@ class World {
       movableObject.width,
       movableObject.height
     );
+    if (movableObject.otherDirection) {
+      this.ctx.restore();
+      movableObject.x = movableObject.x * -1;
+    }
   }
 }
