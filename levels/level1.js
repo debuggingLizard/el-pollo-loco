2c8665bde255835b514073df9f251ebe5ae1b831
Lisa-Marie Maliga <lisa.maliga@outlook.com>
2024-11-15 13:52:32 +0100

    insert coin

--- levels/level1.js
+++ levels/level1.js
@@ -23,21 +23,48 @@ const level1 = new Level(
     new Bottle(1180),
     new Bottle(1210),
   ],
-  [],
+  [new Coin(340, 200)],
   [new Chicken(), new Chicken(), new Chicken(), new Endboss()],
   [new Cloud()],
   [
     new BackgroundObject("../img/5_background/layers/air.png", -80),
     new BackgroundObject("../img/5_background/layers/air.png", 1359),
     new BackgroundObject("../img/5_background/layers/air.png", 2798),
-    new BackgroundObject("../img/5_background/layers/3_third_layer/full.png", -80),
-    new BackgroundObject("../img/5_background/layers/3_third_layer/full.png", 1360),
-    new BackgroundObject("../img/5_background/layers/3_third_layer/full.png", 2800),
-    new BackgroundObject("../img/5_background/layers/2_second_layer/full.png", -80),
-    new BackgroundObject("../img/5_background/layers/2_second_layer/full.png", 1360),
-    new BackgroundObject("../img/5_background/layers/2_second_layer/full.png", 2800),
-    new BackgroundObject("../img/5_background/layers/1_first_layer/full.png", -80),
-    new BackgroundObject("../img/5_background/layers/1_first_layer/full.png", 1360),
-    new BackgroundObject("../img/5_background/layers/1_first_layer/full.png", 2800)
+    new BackgroundObject(
+      "../img/5_background/layers/3_third_layer/full.png",
+      -80
+    ),
+    new BackgroundObject(
+      "../img/5_background/layers/3_third_layer/full.png",
+      1360
+    ),
+    new BackgroundObject(
+      "../img/5_background/layers/3_third_layer/full.png",
+      2800
+    ),
+    new BackgroundObject(
+      "../img/5_background/layers/2_second_layer/full.png",
+      -80
+    ),
+    new BackgroundObject(
+      "../img/5_background/layers/2_second_layer/full.png",
+      1360
+    ),
+    new BackgroundObject(
+      "../img/5_background/layers/2_second_layer/full.png",
+      2800
+    ),
+    new BackgroundObject(
+      "../img/5_background/layers/1_first_layer/full.png",
+      -80
+    ),
+    new BackgroundObject(
+      "../img/5_background/layers/1_first_layer/full.png",
+      1360
+    ),
+    new BackgroundObject(
+      "../img/5_background/layers/1_first_layer/full.png",
+      2800
+    ),
   ]
 );
--- models/coin.class.js
+++ models/coin.class.js
@@ -1,3 +1,11 @@
 class Coin extends Collectable {
-    
+  height = 100;
+  width = 100;
+
+  constructor(x, y) {
+    super();
+    this.x = x;
+    this.y = y;
+    this.loadImage("../img/8_coin/coin_1.png")
+  }
 }
